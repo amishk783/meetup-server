f4e9cff175f7c48df90b8724f69c5811bca38629
amishk783 <amishkumar800@gmail.com>
2023-12-18 19:18:24 +0530

    profile update

--- app.js
+++ app.js
@@ -4,23 +4,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const session = require("express-session");
-const sequelize = require("./util/database")
-  const morgan= require('morgan');
+const sequelize = require("./util/database");
+const morgan = require("morgan");
 
 const SequelizeStore = require("express-session-sequelize")(session.Store);
 
-
-
-
-require('dotenv').config();
+require("dotenv").config();
 
 const authRouter = require("./routes/auth");
-const meetupRouter = require("./routes/meetup")
+const meetupRouter = require("./routes/meetup");
 const meetupController = require("./Controllers/meetup");
-const verifyJWT = require('./middleware/verifyJWT')
+const verifyJWT = require("./middleware/verifyJWT");
 const User = require("./Modals/User");
 const Meetup = require("./Modals/Meetup");
-
 
 const app = express();
 
@@ -28,14 +24,10 @@ app.use(bodyParser.json());
 
 app.use(cors());
 // app.use(session({ secret: "secret-key", resave: false, saveUninitialized: false ,store:sessionStore}));
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-
-
-app.use("/users",authRouter);
+app.use("/users", authRouter);
 app.use("/meetup", meetupRouter);
-
-
 
 app.use((req, res) => {
   res.status(404).send("<h1>Page not found</h1>");
@@ -49,8 +41,8 @@ sequelize
   .then((result) => {
     // console.log(result);
     const server = http.createServer(app);
-
-    server.listen(3003);
+    const PORT = process.env.PORT || 3003;
+    server.listen(PORT);
   })
   .catch((err) => {
     console.log(err);
