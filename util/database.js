7c351c27fdeb5a9d4c540b3f66e0484242799654
amishk783 <amishkumar800@gmail.com>
2024-02-12 21:55:21 +0530

    fixed bugs

--- util/database.js
+++ util/database.js
@@ -1,9 +1,10 @@
 const Sequelize = require("sequelize");
 
+let sequelize;
 if (process.env.JAWSDB_URL) {
-  const sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  const sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
